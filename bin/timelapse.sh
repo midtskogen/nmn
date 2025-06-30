ba761f89f8b1b9822adad4b3d5e60121709915f1
Steinar Midtskogen <steinar@latinitas.org>
2025-06-30 09:51:08 +0000

    New timelapse script

--- bin/timelapse.sh
+++ bin/timelapse.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-for i in /meteor/cam[0-9]; do
-    cd $i/$(date -u +%Y%m%d -d yesterday)
-    mencoder -mf fps=30:type=jpg 'mf://[0-2][0-9][0-5][0-9][0-5][0-9].jpg' -vf eq2=1.67,pp=hb/vb/dr/tn:0:256:512 -of rawvideo -ovc x264 -x264encopts crf=26 -o timelapse.264 2> /dev/null > /dev/null && (rm -f timelapse.mp4; MP4Box timelapse.mp4 -add timelapse.264 -fps 30 2> /dev/null; rm -f timelapse.264 [0-2][0-9][0-5][0-9][0-5][0-9].jpg)
+for i in /remote/cam[1-7]; do
+    src_date=$(date -u +%Y%m%d -d yesterday)
+    src_dir="$i/$src_date"
+
+    # Replace "/remote/" with "/meteor/timelapse/"
+    out_dir="${i/\/remote\//\/meteor\/timelapse\/}/$src_date"
+    mkdir -p "$out_dir"
+
+    # Generate comma-separated list of image files
+    file_list=$(find "$src_dir" -regex '.*[0-2][0-9]/full_[0-5][0-9]\.jpg' | sort | tr '\n' ',' | sed 's/,$//')
+
+    # Only proceed if file_list is non-empty
+    if [ -n "$file_list" ]; then
+        mencoder -mf fps=30:type=jpg mf://$file_list \
+            -vf eq2=1.67,pp=hb/vb/dr/tn:0:256:512 \
+            -of rawvideo -ovc x264 -x264encopts crf=26 -o "$out_dir/timelapse.264"
+
+        MP4Box "$out_dir/timelapse.mp4" -add "$out_dir/timelapse.264" -fps 30 2> /dev/null
+        rm -f "$out_dir/timelapse.264"
+    fi
 done

b23a4edeb1369018f694e43cbe713b5dc2a0ed97
Steinar Midtskogen <steinar@latinitas.org>
2025-11-24 10:04:43 +0100

    More fixes to cleanup

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-find /meteor/cam*/*.3gp /meteor/cam*/cifs* /tmp/tmp* -mtime +1 -exec rm -f {} \; 2> /dev/null
 find /meteor/cam*/amsevents -depth -type d -mtime +180 -exec rm -r {} \; 2> /dev/null
 
 ~/bin/cleancam.py
 
 for x in $(cat /etc/cameras.id); do
-    find /meteor/cam$x -empty -delete
+    touch /meteor/cam$x/metdetect.log /meteor/cam$x/cammon.log
     sed -i -e :a -e '$q;N;100001,$D;ba' /meteor/cam$x/metdetect.log
     sed -i -e :a -e '$q;N;100001,$D;ba' /meteor/cam$x/cammon.log
+    find /meteor/cam$x/ -empty -delete
 done

9299062cef04c80b07c3bda6a49ee6db8a3ed635
Steinar Midtskogen <steinar@latinitas.org>
2026-07-11 10:53:25 +0200

    Adjust quality of hires timelapses

--- bin/timelapse_eq.sh
+++ bin/timelapse_eq.sh
@@ -7,7 +7,7 @@ rm -f /tmp/frames_eq.txt
 
 find "$DIR" -name 'full_*.jpg' | sort | awk '{print "file \047" $0 "\047"}' > /tmp/frames_eq_hires.txt
 if [ -s /tmp/frames_eq_hires.txt ]; then
-    ffmpeg -f concat -safe 0 -r 30 -i /tmp/frames_eq_hires.txt -crf 22 -c:v libx264 -pix_fmt yuv420p -y "$DIR/timelapse_hires_new.mp4" \
+    ffmpeg -f concat -safe 0 -r 30 -i /tmp/frames_eq_hires.txt -crf 25 -c:v libx264 -pix_fmt yuv420p -y "$DIR/timelapse_hires_new.mp4" \
       && mv "$DIR/timelapse_hires_new.mp4" "$DIR/timelapse_hires.mp4"
 fi
 rm -f /tmp/frames_eq_hires.txt
--- bin/timelapse_fe.sh
+++ bin/timelapse_fe.sh
@@ -7,7 +7,7 @@ rm -f /tmp/frames_fe.txt
 
 find "$DIR" -name 'full_*.jpg' | sort | awk '{print "file \047" $0 "\047"}' > /tmp/frames_fe_hires.txt
 if [ -s /tmp/frames_fe_hires.txt ]; then
-    ffmpeg -f concat -safe 0 -r 30 -i /tmp/frames_fe_hires.txt -crf 22 -c:v libx264 -pix_fmt yuv420p -y "$DIR/timelapse_hires_new.mp4" \
+    ffmpeg -f concat -safe 0 -r 30 -i /tmp/frames_fe_hires.txt -crf 25 -c:v libx264 -pix_fmt yuv420p -y "$DIR/timelapse_hires_new.mp4" \
       && mv "$DIR/timelapse_hires_new.mp4" "$DIR/timelapse_hires.mp4"
 fi
 rm -f /tmp/frames_fe_hires.txt
